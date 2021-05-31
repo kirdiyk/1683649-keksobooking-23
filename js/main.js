21018a65344135289f0fdf7958032d81f46e3078
kirdiyk <kirka.aka112@ya.ru>
2021-05-31 22:01:17 +0300

    after code review

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-function checkValue (firstValue, secondValue) {
-  return ((firstValue < 0 && secondValue < 0) || (secondValue < firstValue)) ? false : true;
-}
+const checkValue = (firstValue, secondValue) => (!((firstValue < 0 && secondValue < 0) || (secondValue < firstValue)));
 
-function getRandomRange (firstValue, secondValue, fixValue) {
+//for integer use two parametrs,for float use 3 paramerts
+const getRandomRangeIntOrFloat = (firstValue, secondValue, fixValue) => {
   if (checkValue (firstValue, secondValue)) {
     let number = (Math.random() * (secondValue - firstValue))+firstValue;
     const numberAbs = Math.abs(number);
+    // eslint-disable-next-line no-nested-ternary
     (number < 0) && (numberAbs <= secondValue) ? (number = numberAbs) : (number >= 0) && (number <= secondValue) ? number : number = secondValue;
-    if (fixValue > 0) {
-      return [number.toFixed(fixValue), fixValue];
-    }
     return number.toFixed(fixValue);
   }
   return 'Значения не могут быть отрицательными и диaпазон должен быть указан в формате: (Меньшее число, Большее число, (при необходимости число равное количеству знаков после запятой)';
-}
+};
 
-getRandomRange (2, 3);
+getRandomRangeIntOrFloat (2, -0.94, 1);
 
 
